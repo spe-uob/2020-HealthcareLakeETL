9997cf73fdc927bb6f80d3efa4b330411c96d6ee
joekendal <13680617+joekendal@users.noreply.github.com>
2021-04-14 13:03:16 +0100

    Formatting

--- tests/test_patient.py
+++ tests/test_patient.py
@@ -6,25 +6,27 @@ from main import map_patient
 class TestPatient():
 
     __nullable_fields = [
-        "race_concept_id",
-        "ethnicity_concept_id",
-        "location_id"
+        "race_concept_id", "ethnicity_concept_id",
+        "location_id", "care_site_id"
     ]
     expected_fields = [
-        "person_id", "provider_id", "care_site_id",
-        "gender_concept_id", "year_of_birth", "month_of_birth",
-        "day_of_birth", "birth_datetime"
+        "person_id", "provider_id", "gender_concept_id",
+        "year_of_birth", "month_of_birth", "day_of_birth", "birth_datetime"
     ] + __nullable_fields
 
     @pytest.fixture()
     def set_up(self, data_frame):
+        # Using data/catalog.parquet for tests
         yield map_patient(data_frame)
 
     def test_field_names(self, set_up):
         actual_fields = set_up.columns
         # Test whether column names exist in dataframe
-        assert(set(actual_fields) == set(self.expected_fields)
-               ), "Resulting columns did not match the expected columns: %s" % self.expected_fields
+        assert(
+            set(actual_fields) == set(self.expected_fields)
+        ), \
+            "Resulting columns did not match the expected columns: %s"\
+            % self.expected_fields
 
     def test_field_types(self, set_up):
         # TODO: test column data types
