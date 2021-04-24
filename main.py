8f9ef47e2598a36237a8b744fcd40dd8d2f8d0b9
joekendal <13680617+joekendal@users.noreply.github.com>
2021-04-24 16:50:40 +0100

    Added name-value arguments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from os import environ
+import sys
 
 from mappings import (map_patient)
 
@@ -6,29 +6,40 @@ from pyspark.context import SparkContext
 
 from awsglue.context import GlueContext
 from awsglue.dynamicframe import DynamicFrame
+from awsglue.utils import getResolvedOptions
+from awsglue.job import Job
 
 
-glueContext = GlueContext(SparkContext.getOrCreate())
+args = getResolvedOptions(sys.argv, ["DB_NAME", "TBL_NAME", "OUT_DIR"])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(job_name="FHIR->OMOP", args=args)
 
 # catalog: database and table names
-db_name = environ.get('DB_NAME')
-tbl_name = environ.get('TBL_NAME')
+db_name = args["DB_NAME"]
+tbl_name = args['TBL_NAME']
 
 # output s3
-output_dir = environ.get('OUT_DIR')
+output_dir = args['OUT_DIR']
 
 # Create dynamic frame from source table
-fhir = glueContext.create_dynamic_frame_from_catalog(
-    database=db_name, table_name=tbl_name
+datasource = glueContext.create_dynamic_frame_from_catalog(
+    database=db_name, table_name=tbl_name, transformation_ctx="datasource"
 )
 
 # Convert to DataFrame
-df = fhir.toDF()
+df = datasource.toDF()
 
 # Perform mappings
 person = DynamicFrame.fromDF(map_patient(df), glueContext, "person")
 
 # Write Parquet to S3
-glueContext.write_dynamic_frame.from_options(
-    frame=person, connection_type="s3", connection_options={"path": output_dir}, format="parquet"
+datasink = glueContext.write_dynamic_frame.from_options(
+    frame=person, connection_type="s3", connection_options={"path": output_dir},
+    format="parquet", transformation_ctx="datasink"
 )
+
+job.commit()
