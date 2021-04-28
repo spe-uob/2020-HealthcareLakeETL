248cd635be98854e19960820a3ffae33d8105c09
joekendal <13680617+joekendal@users.noreply.github.com>
2021-04-28 02:25:37 +0100

    Gets rid of C extensions (incompatible with Glue)

--- mappings/observation.py
+++ mappings/observation.py
@@ -1,4 +1,3 @@
-import datetime as dt
 import pyspark.sql.functions as F
 
 
@@ -14,31 +13,32 @@ def map_observation(df):
     filtered = df.filter(df['resourceType'] == 'Observation')
     Observation = filtered.filter(filtered.valueCodeableConcept.isNotNull())
     Observation = Observation.select(['id',
-                                'subject',
-                                'code',
-                                'performer',
-                                'encounter',
-                                'meta',
-                                'effectiveDateTime',
-                                'valueCodeableConcept',
-                                'category'])
+                                      'subject',
+                                      'code',
+                                      'performer',
+                                      'encounter',
+                                      'meta',
+                                      'effectiveDateTime',
+                                      'valueCodeableConcept',
+                                      'category'])
 
     split_dates = F.split(Observation["effectiveDateTime"], 'T')
 
     Observation = Observation.withColumnRenamed("id", "observation_id")\
-                            .withColumn("observation_type_concept_id", Observation.category.coding.getItem(0).code.getItem(0))\
-                            .withColumn("observation_date", split_dates.getItem(0))\
-                            .withColumn("person_id", Observation.subject.reference)\
-                            .withColumn("value_as_string", Observation.valueCodeableConcept.text)\
-                            .withColumnRenamed("code", "observation_concept_id")\
-                            .withColumnRenamed("effectiveDateTime", "observation_datetime")\
-                            .drop("valueCodeableConcept")\
-                            .withColumn("visit_occurrence_id", Observation.encounter.reference)\
-                            .withColumnRenamed("performer", "provider_id")\
-                            .drop("encounter")\
-                            .drop("subject")\
-                            .drop("meta")\
-                            .drop("category")
+        .withColumn("observation_type_concept_id", Observation.category.coding.getItem(0).code.getItem(0))\
+        .withColumn("observation_date", split_dates.getItem(0))\
+        .withColumn("person_id", Observation.subject.reference)\
+        .withColumn("value_as_string", Observation.valueCodeableConcept.text)\
+        .withColumnRenamed("code", "observation_concept_id")\
+        .withColumnRenamed("effectiveDateTime", "observation_datetime")\
+        .drop("valueCodeableConcept")\
+        .withColumn("visit_occurrence_id", Observation.encounter.reference)\
+        .withColumnRenamed("performer", "provider_id")\
+        .drop("encounter")\
+        .drop("subject")\
+        .drop("meta")\
+        .drop("category")
 
-    Observation = Observation.withColumn("observation_concept_id", Observation.observation_concept_id.coding.getItem(0).code)
+    Observation = Observation.withColumn(
+        "observation_concept_id", Observation.observation_concept_id.coding.getItem(0).code)
     return Observation
